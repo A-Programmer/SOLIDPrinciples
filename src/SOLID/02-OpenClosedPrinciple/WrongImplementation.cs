1986b46ae7a832cc8e17d01b8314da4caa567eb1
Kamran Sadin <mrsadin@gmail.com>
2022-01-18 09:28:01 +0330

    02. Open Closed Principle

---
+++ src/SOLID/02-OpenClosedPrinciple/WrongImplementation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace SOLID.OpenClosedPrinciple
+{
+    public class RectangleShape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+
+    public class CircleShape
+    {
+        public double Radius { get; set; }
+    }
+
+    public class AreaCalculator
+    {
+        public double Calculate(object shape)
+        {
+            if (shape is RectangleShape)
+            {
+                RectangleShape rectangle = (RectangleShape)shape;
+                return rectangle.Width * rectangle.Height;
+            }
+            else
+            {
+                CircleShape circle = (CircleShape)shape;
+                return circle.Radius * circle.Radius * 3.14;
+            }
+        }
+    }
+
+}
